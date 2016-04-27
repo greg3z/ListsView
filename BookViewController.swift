a4467b814a6cae900810c4c2e0905e85107801df
Grégoire Lhotellier <glhotellier@gmail.com>
2016-04-27 15:57:16 +0200

    Using composition over inheritance on BookViewController

--- BookViewController.swift
+++ BookViewController.swift
@@ -8,14 +8,14 @@
 
 import UIKit
 
-class BookViewController<Element>: MultiPageViewController {
+class BookViewController<Element>: UIViewController {
     
-    var bookPageViewControllers: [PageViewController<Element>] {
-        return pageViewControllers as! [PageViewController]
-    }
+    let book: Book<Element>
+    let cellTypeForElement: Element -> UITableViewCell.Type
+    var pageViewControllers = [PageViewController<Element>]()
     var refreshCallback: (Void -> Void)? {
         didSet {
-            for pageViewController in bookPageViewControllers {
+            for pageViewController in pageViewControllers {
                 pageViewController.refreshCallback = refreshCallback
             }
         }
@@ -23,45 +23,46 @@ class BookViewController<Element>: MultiPageViewController {
     var elementTouched: ((Element, UITableViewCell) -> Void)?
     var configureCell: ((Element, cell: UITableViewCell, tableView: UITableView, indexPath: NSIndexPath) -> Void)? {
         didSet {
-            for pageViewController in bookPageViewControllers {
+            for pageViewController in pageViewControllers {
                 pageViewController.configureCell = configureCell
             }
         }
     }
     
     init(book: Book<Element>, cellTypeForElement: Element -> UITableViewCell.Type) {
-        var pageViewControllers = [PageViewController<Element>]()
-        for page in book.pages {
-            let pageViewController = PageViewController(page: page, cellTypeForElement: cellTypeForElement)
-            pageViewControllers.append(pageViewController)
-        }
-        super.init(pageViewControllers: pageViewControllers)
+        self.book = book
+        self.cellTypeForElement = cellTypeForElement
+        super.init(nibName: nil, bundle: nil)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        for pageViewController in bookPageViewControllers {
-            pageViewController.elementTouched = {
-                [weak self] element, cell in
-                self?.elementTouched?(element, cell)
-            }
+        for page in book.pages {
+            let pageViewController = PageViewController(page: page, cellTypeForElement: cellTypeForElement)
+            pageViewController.elementTouched = elementTouched
+            pageViewController.configureCell = configureCell
+            pageViewControllers.append(pageViewController)
         }
+        let multiPageViewController = MultiPageViewController(pageViewControllers: pageViewControllers)
+        multiPageViewController.view.frame = view.bounds
+        view.addSubview(multiPageViewController.view)
+        addChildViewController(multiPageViewController)
     }
     
     func reloadVisibleCells() {
-        for pageViewController in bookPageViewControllers {
+        for pageViewController in pageViewControllers {
             pageViewController.reloadVisibleCells()
         }
     }
     
     func reloadData() {
-        for pageViewController in bookPageViewControllers {
+        for pageViewController in pageViewControllers {
             pageViewController.tableView.reloadData()
         }
     }
     
     func endRefreshing() {
-        for pageViewController in bookPageViewControllers {
+        for pageViewController in pageViewControllers {
             pageViewController.refreshControl?.endRefreshing()
         }
     }
